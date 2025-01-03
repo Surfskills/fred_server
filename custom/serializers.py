b3d64fb85d561fcb5cceb22e25fe1ab5d0c047a9
Surfskills <ojjfred@gmail.com>
2025-01-03 21:12:28 +0300

    db fixed

--- custom/serializers.py
+++ custom/serializers.py
@@ -4,7 +4,18 @@ from .models import SoftwareRequest, ResearchRequest
 class BaseRequestSerializer(serializers.ModelSerializer):
     class Meta:
         abstract = True
-        fields = ('id', 'title', 'project_description', 'request_type', 'user', 'created_at', 'updated_at')
+        fields = (
+            'id', 
+            'title', 
+            'project_description', 
+            'request_type', 
+            'user', 
+            'status',
+            'payment_status',
+            'order_status',
+            'created_at', 
+            'updated_at'
+        )
         read_only_fields = ('user', 'created_at', 'updated_at')
 
     def validate(self, attrs):
@@ -18,9 +29,14 @@ class SoftwareRequestSerializer(BaseRequestSerializer):
     class Meta(BaseRequestSerializer.Meta):
         model = SoftwareRequest
         fields = BaseRequestSerializer.Meta.fields + (
-            'budget_range', 'timeline', 'frontend_languages', 
-            'frontend_frameworks', 'backend_languages', 
-            'backend_frameworks', 'ai_languages', 'ai_frameworks'
+            'budget_range',
+            'timeline',
+            'frontend_languages',
+            'frontend_frameworks',
+            'backend_languages',
+            'backend_frameworks',
+            'ai_languages',
+            'ai_frameworks'
         )
 
     def create(self, validated_data):
@@ -32,14 +48,22 @@ class ResearchRequestSerializer(BaseRequestSerializer):
     class Meta(BaseRequestSerializer.Meta):
         model = ResearchRequest
         fields = BaseRequestSerializer.Meta.fields + (
-            'academic_writing_type', 'writing_technique', 
-            'research_paper_structure', 'academic_writing_style',
-            'research_paper_writing_process', 'critical_writing_type',
-            'critical_thinking_skill', 'critical_writing_structure',
-            'discussion_type', 'discussion_component',
-            'academic_writing_tool', 'research_paper_database',
-            'plagiarism_checker', 'reference_management_tool',
-            'academic_discussion_type', 'citation_style'
+            'academic_writing_type',
+            'writing_technique',
+            'research_paper_structure',
+            'academic_writing_style',
+            'research_paper_writing_process',
+            'critical_writing_type',
+            'critical_thinking_skill',
+            'critical_writing_structure',
+            'discussion_type',
+            'discussion_component',
+            'academic_writing_tool',
+            'research_paper_database',
+            'plagiarism_checker',
+            'reference_management_tool',
+            'academic_discussion_type',
+            'citation_style'
         )
 
     def create(self, validated_data):
@@ -48,39 +72,52 @@ class ResearchRequestSerializer(BaseRequestSerializer):
         return super().create(validated_data)
 
 class RequestListSerializer(serializers.Serializer):
+    # Base fields
     id = serializers.IntegerField()
     title = serializers.CharField()
     project_description = serializers.CharField()
     request_type = serializers.CharField()
+    status = serializers.CharField()
+    payment_status = serializers.CharField()
+    order_status = serializers.CharField()
     created_at = serializers.DateTimeField()
     updated_at = serializers.DateTimeField()
     user = serializers.PrimaryKeyRelatedField(read_only=True)
 
+    # Software request specific fields
+    budget_range = serializers.CharField(required=False)
+    timeline = serializers.CharField(required=False)
+    frontend_languages = serializers.CharField(required=False)
+    frontend_frameworks = serializers.CharField(required=False)
+    backend_languages = serializers.CharField(required=False)
+    backend_frameworks = serializers.CharField(required=False)
+    ai_languages = serializers.CharField(required=False)
+    ai_frameworks = serializers.CharField(required=False)
+
+    # Research request specific fields
+    academic_writing_type = serializers.CharField(required=False)
+    writing_technique = serializers.CharField(required=False)
+    research_paper_structure = serializers.CharField(required=False)
+    academic_writing_style = serializers.CharField(required=False)
+    research_paper_writing_process = serializers.CharField(required=False)
+    critical_writing_type = serializers.CharField(required=False)
+    critical_thinking_skill = serializers.CharField(required=False)
+    critical_writing_structure = serializers.CharField(required=False)
+    discussion_type = serializers.CharField(required=False)
+    discussion_component = serializers.CharField(required=False)
+    academic_writing_tool = serializers.CharField(required=False)
+    research_paper_database = serializers.CharField(required=False)
+    plagiarism_checker = serializers.CharField(required=False)
+    reference_management_tool = serializers.CharField(required=False)
+    academic_discussion_type = serializers.CharField(required=False)
+    citation_style = serializers.CharField(required=False)
+
     def to_representation(self, instance):
         if isinstance(instance, SoftwareRequest):
             serializer = SoftwareRequestSerializer(instance)
-            data = serializer.data
-            # Ensure only common fields are returned
-            return {
-                'id': data['id'],
-                'title': data['title'],
-                'project_description': data['project_description'],
-                'request_type': data['request_type'],
-                'created_at': data['created_at'],
-                'updated_at': data['updated_at'],
-                'user': data['user']
-            }
         elif isinstance(instance, ResearchRequest):
             serializer = ResearchRequestSerializer(instance)
-            data = serializer.data
-            # Ensure only common fields are returned
-            return {
-                'id': data['id'],
-                'title': data['title'],
-                'project_description': data['project_description'],
-                'request_type': data['request_type'],
-                'created_at': data['created_at'],
-                'updated_at': data['updated_at'],
-                'user': data['user']
-            }
-        return super().to_representation(instance) 
+        else:
+            return super().to_representation(instance)
+        
+        return serializer.data 

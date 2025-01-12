f3dbbf0b1b11490e0befddd909bfdba4948fdd2b
Surfskills <ojjfred@gmail.com>
2025-01-12 15:29:28 +0300

    signals updated

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -49,4 +49,4 @@ class SignInSerializer(serializers.Serializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active']
+        fields = ['id', 'email', 'first_name', 'last_name', 'is_staff', 'is_active']

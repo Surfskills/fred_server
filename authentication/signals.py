701976ec9a0ff2c0ee9b1354c32bb68fa79ac97b
Surfskills <ojjfred@gmail.com>
2025-01-12 15:25:27 +0300

    signals updated

--- authentication/signals.py
+++ authentication/signals.py
@@ -1,4 +1,3 @@
-# authentication/signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import User, Profile
@@ -12,4 +11,9 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     """Save the user's profile when the user is saved."""
-    instance.profile.save()
+    # Ensure the user has a profile before saving
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
+    else:
+        # If no profile exists, create it
+        Profile.objects.create(user=instance)

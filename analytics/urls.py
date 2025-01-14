544aa9d10bd38c2391a1c0bd5ba2074bb2356ae9
Surfskills <ojjfred@gmail.com>
2025-01-14 12:37:10 +0300

    analytics

--- analytics/urls.py
+++ analytics/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import OrderAnalyticsView, GeneralAnalyticsView
 
 urlpatterns = [
-    path('/order-analytics/<str:service_id>/', OrderAnalyticsView.as_view(), name='order_analytics'),
-    path('/order-analytics/<str:service_id>/<str:type>/pdf/', OrderAnalyticsView.as_view(), name='order_pdf'),
-    path('/general-analytics/', GeneralAnalyticsView.as_view(), name='general_analytics'),
+    path('order-analytics/<str:service_id>/', OrderAnalyticsView.as_view(), name='order_analytics'),
+    path('order-analytics/<str:service_id>/<str:type>/pdf/', OrderAnalyticsView.as_view(), name='order_pdf'),
+    path('general-analytics/', GeneralAnalyticsView.as_view(), name='general_analytics'),
 ]

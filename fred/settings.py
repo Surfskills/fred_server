b98b510ef5ce4f164339caba1f4bfeab2776c370
Surfskills <ojjfred@gmail.com>
2025-03-01 20:44:36 +0300

    xtension added

--- fred/settings.py
+++ fred/settings.py
@@ -37,12 +37,12 @@ DATABASES = {
     )
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # SQLite database file stored in the project root
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',  # SQLite database file stored in the project root
+#     }
+# }
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/

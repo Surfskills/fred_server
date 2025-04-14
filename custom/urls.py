9d6c1d2e0fc5fdfffb73e1bea129652fcf489185
Surfskills <ojjfred@gmail.com>
2025-04-14 18:35:10 +0300

    Migrations owrk

--- custom/urls.py
+++ custom/urls.py
@@ -3,8 +3,10 @@ from rest_framework.routers import DefaultRouter
 from .views import RequestViewSet
 
 router = DefaultRouter()
-router.register(r'', RequestViewSet, basename='request')
+router.register(r'requests', RequestViewSet, basename='request')
 
+# Manually add URL for shared_id
 urlpatterns = [
+    path('requests/<str:shared_id>/', RequestViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}), name='request-detail'),
     path('', include(router.urls)),
 ]

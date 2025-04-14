dd235dcf4b627251b884d5b1228a933158f77684
Surfskills <ojjfred@gmail.com>
2025-04-14 19:13:28 +0300

    Migrations owrk

--- custom/views.py
+++ custom/views.py
@@ -1,4 +1,3 @@
-from django.http import Http404
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -14,19 +13,10 @@ from .serializers import (
 )
 
 
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import action
-from rest_framework.exceptions import PermissionDenied
-from django.http import Http404
-from django.db.models import Value, CharField
-from .models import SoftwareRequest, ResearchRequest
-from .serializers import SoftwareRequestSerializer, ResearchRequestSerializer
-
 class RequestViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+    lookup_field = 'shared_id'  # Define shared_id as the lookup field
 
     def get_queryset(self, request_type=None):
         user = self.request.user
@@ -35,29 +25,72 @@ class RequestViewSet(viewsets.ViewSet):
             return SoftwareRequest.objects.filter(user=user)
         elif request_type == 'research':
             return ResearchRequest.objects.filter(user=user)
-
-        # Merge software and research requests
+        
+        # Ensure user filtering for both querysets
         software_requests = SoftwareRequest.objects.filter(user=user).annotate(
             model_type=Value('software', output_field=CharField()),
-            # Add NULLs for research-specific fields
+            # Add NULL values for research-specific fields
             academic_writing_type=Value(None, output_field=CharField(null=True)),
-            # ... (you can leave the rest as-is)
-        ).values(...)
-
+            writing_technique=Value(None, output_field=CharField(null=True)),
+            academic_writing_style=Value(None, output_field=CharField(null=True)),
+            critical_writing_type=Value(None, output_field=CharField(null=True)),
+            critical_thinking_skill=Value(None, output_field=CharField(null=True)),
+            critical_writing_structure=Value(None, output_field=CharField(null=True)),
+            discussion_type=Value(None, output_field=CharField(null=True)),
+            discussion_component=Value(None, output_field=CharField(null=True)),
+            citation_style=Value(None, output_field=CharField(null=True)),
+            number_of_pages=Value(None, output_field=CharField(null=True)),
+            number_of_references=Value(None, output_field=CharField(null=True)),
+            study_level=Value(None, output_field=CharField(null=True))
+        ).values(
+            'id', 'shared_id', 'title', 'project_description', 'request_type',
+            'created_at', 'updated_at', 'user_id', 'model_type',
+            'status', 'payment_status', 'order_status', 'cost',
+            'budget_range', 'timeline', 'frontend_languages',
+            'frontend_frameworks', 'backend_languages', 'backend_frameworks',
+            'ai_languages', 'ai_frameworks',
+            'academic_writing_type', 'writing_technique',
+            'academic_writing_style', 'critical_writing_type',
+            'critical_thinking_skill', 'critical_writing_structure',
+            'discussion_type', 'discussion_component',
+            'citation_style', 'number_of_pages', 'number_of_references',
+            'study_level'
+        )
+        
         research_requests = ResearchRequest.objects.filter(user=user).annotate(
             model_type=Value('research', output_field=CharField()),
-            # Add NULLs for software-specific fields
+            # Add NULL values for software-specific fields
             budget_range=Value(None, output_field=CharField(null=True)),
-            # ... (you can leave the rest as-is)
-        ).values(...)
-
+            timeline=Value(None, output_field=CharField(null=True)),
+            frontend_languages=Value(None, output_field=CharField(null=True)),
+            frontend_frameworks=Value(None, output_field=CharField(null=True)),
+            backend_languages=Value(None, output_field=CharField(null=True)),
+            backend_frameworks=Value(None, output_field=CharField(null=True)),
+            ai_languages=Value(None, output_field=CharField(null=True)),
+            ai_frameworks=Value(None, output_field=CharField(null=True))
+        ).values(
+            'id', 'shared_id', 'title', 'project_description', 'request_type',
+            'created_at', 'updated_at', 'user_id', 'model_type',
+            'status', 'payment_status', 'order_status', 'cost',
+            'budget_range', 'timeline', 'frontend_languages',
+            'frontend_frameworks', 'backend_languages', 'backend_frameworks',
+            'ai_languages', 'ai_frameworks',
+            'academic_writing_type', 'writing_technique',
+            'academic_writing_style', 'critical_writing_type',
+            'critical_thinking_skill', 'critical_writing_structure',
+            'discussion_type', 'discussion_component',
+            'citation_style', 'number_of_pages', 'number_of_references',
+            'study_level'
+        )
+        
         return software_requests.union(research_requests).order_by('-created_at')
 
     def get_object_by_shared_id(self, shared_id):
         """
-        Look up a request (software or research) by shared_id and ensure it belongs to the user.
+        Helper method to get an object by shared_id, ensuring it belongs to the current user
         """
         user = self.request.user
+        
         try:
             obj = SoftwareRequest.objects.get(shared_id=shared_id, user=user)
             return obj, SoftwareRequestSerializer
@@ -74,63 +107,98 @@ class RequestViewSet(viewsets.ViewSet):
         return Response(list(queryset), status=status.HTTP_200_OK)
 
     def create(self, request):
-        """Create a new request"""
+        """Create a new request with a unique ID"""
         request_type = request.data.get('request_type')
+        
         if not request_type:
-            return Response({'error': 'request_type is required'}, status=status.HTTP_400_BAD_REQUEST)
-
+            return Response(
+                {'error': 'request_type is required'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        # Add user to request data
         data = request.data.copy()
         data['user'] = request.user.id
-
+        
         if request_type == 'software':
-            serializer = SoftwareRequestSerializer(data=data, context={'request': request})
+            serializer = SoftwareRequestSerializer(
+                data=data,
+                context={'request': request}
+            )
         elif request_type == 'research':
-            serializer = ResearchRequestSerializer(data=data, context={'request': request})
+            serializer = ResearchRequestSerializer(
+                data=data,
+                context={'request': request}
+            )
         else:
-            return Response({'error': f'Invalid request type: {request_type}'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {'error': f'Invalid request type: {request_type}. Allowed types: software, research'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
         if serializer.is_valid():
-            serializer.save(user=request.user)
+            instance = serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response({'error': 'Invalid data', 'details': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            
+        return Response(
+            {'error': 'Invalid data provided', 'details': serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
     def retrieve(self, request, shared_id=None):
-        """Retrieve a request by shared_id"""
+        """Get a specific request by shared_id"""
         instance, serializer_class = self.get_object_by_shared_id(shared_id)
         serializer = serializer_class(instance)
         return Response(serializer.data)
 
     def update(self, request, shared_id=None):
-        """Update a request by shared_id"""
+        """Update a specific request by shared_id"""
         instance, serializer_class = self.get_object_by_shared_id(shared_id)
-        serializer = serializer_class(instance, data=request.data, partial=True, context={'request': request})
+        
+        serializer = serializer_class(
+            instance,
+            data=request.data,
+            context={'request': request},
+            partial=True
+        )
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response({'error': 'Invalid data', 'details': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            
+        return Response(
+            {'error': 'Invalid data provided', 'details': serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
     def can_delete_request(self, instance):
-        """Disallow deleting paid orders"""
+        """
+        Helper method to check if a request can be deleted based on payment status
+        """
         if instance.payment_status == 'paid':
-            raise PermissionDenied("Cannot delete a paid order.")
+            raise PermissionDenied("Cannot delete a paid order. Please contact support if you need assistance.")
         return True
-
+    
     def destroy(self, request, shared_id=None):
-        """Delete a request by shared_id"""
+        """Delete a specific request by shared_id if it hasn't been paid for"""
         instance, _ = self.get_object_by_shared_id(shared_id)
+        
+        # Check if the request can be deleted
         self.can_delete_request(instance)
+        
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     @action(detail=False, methods=['get'])
     def software(self, request):
+        """Get all software requests for the current user"""
         queryset = self.get_queryset(request_type='software')
         serializer = SoftwareRequestSerializer(queryset, many=True)
         return Response(serializer.data)
 
     @action(detail=False, methods=['get'])
     def research(self, request):
+        """Get all research requests for the current user"""
         queryset = self.get_queryset(request_type='research')
         serializer = ResearchRequestSerializer(queryset, many=True)
         return Response(serializer.data)
